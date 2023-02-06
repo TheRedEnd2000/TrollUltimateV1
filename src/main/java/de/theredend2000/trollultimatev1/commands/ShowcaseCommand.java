52b5bd90ad639003740e4e8e39ee15c2da3189a3
TheRedEnd2000 <fkircher2007@gmail.com>
2023-02-06 20:15:02 +0100

    kp1

--- src/main/java/de/theredend2000/trollultimatev1/commands/ShowcaseCommand.java
+++ src/main/java/de/theredend2000/trollultimatev1/commands/ShowcaseCommand.java
@@ -3,10 +3,12 @@ package de.theredend2000.trollultimatev1.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ShowcaseCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
         return false;
     }
 }
